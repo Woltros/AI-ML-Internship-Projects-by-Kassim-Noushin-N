2dd59c681328df5203b471d5467015bb0beff66e
Kassim Noushin N <258039977+Woltros@users.noreply.github.com>
2026-07-23 19:47:45 +0530

    Day 2/

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+name = "Kassim"      # String
+age = 22             # Integer
+height = 172.5       # Float
+student = True       # Boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
---
+++ variables.py
@@ -0,0 +1,7 @@
+name = "Kassim Noushin N"
+age = 22
+cgpa = 8.5
+
+print(name)
+print(age)
+print(cgpa)
